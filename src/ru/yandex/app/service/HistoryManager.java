accffcfcaf6950bf3388c4a471807c046be8737f
Valentine B <vanta11a@yandex.ru>
2024-10-31 17:13:09 +0500

    HistoryManager.java:8:5: 'METHOD_DEF' should be separated from previous line.

--- src/ru/yandex/app/service/HistoryManager.java
+++ src/ru/yandex/app/service/HistoryManager.java
@@ -1,11 +1,12 @@
 package ru.yandex.app.service;
-import ru.yandex.app.model.*;
 
+import ru.yandex.app.model.*;
 import java.util.List;
 
 public interface HistoryManager {
 
     void add(Task task);
+
     void remove(int id);
 
     List<Task> getHistory();
