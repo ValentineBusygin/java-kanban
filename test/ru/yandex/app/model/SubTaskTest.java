6a328064842649f16af33441cec6935e45562057
Valentine B <vanta11a@yandex.ru>
2024-10-31 17:16:19 +0500

    Заменил Assert.* на статичный импорт

--- test/ru/yandex/app/model/SubTaskTest.java
+++ test/ru/yandex/app/model/SubTaskTest.java
@@ -1,20 +1,20 @@
 package ru.yandex.app.model;
 
 import org.junit.jupiter.api.Test;
-import org.testng.Assert;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SubTaskTest {
 
     @Test
     void getEpicId() {
         SubTask newSubTask = new SubTask("getEpicId","getEpicIdDesc", 100500);
-        Assert.assertEquals(newSubTask.getEpicId(), 100500);
+        assertEquals(newSubTask.getEpicId(), 100500);
     }
 
     @Test
     void setEpicId() {
         SubTask newSubTask = new SubTask("getEpicId","getEpicIdDesc", 100500);
         newSubTask.setEpicId(1050);
-        Assert.assertEquals(newSubTask.getEpicId(), 1050);
+        assertEquals(newSubTask.getEpicId(), 1050);
     }
 }
